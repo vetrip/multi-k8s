c86b977a9267f345cea020d0a28958ad1095f74b
pvetrieng@gmail.com <pvetrieng@gmail.com>
2021-02-05 23:50:14 -0800

    refactor

--- employees-ui/src/app/create-employee/create-employee.component.ts
+++ employees-ui/src/app/create-employee/create-employee.component.ts
@@ -1,23 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Employee } from '../employee';
-import { Router } from '@angular/router';
-import { EmployeeService } from '../employee.service';
+import { Component, OnInit } from "@angular/core";
+import { Employee } from "../employee";
+import { Router } from "@angular/router";
+import { EmployeeService } from "../employee.service";
 
 @Component({
-  selector: 'app-create-employee',
-  templateUrl: './create-employee.component.html',
-  styleUrls: ['./create-employee.component.css']
+  selector: "app-create-employee",
+  templateUrl: "./create-employee.component.html",
+  styleUrls: ["./create-employee.component.css"],
 })
 export class CreateEmployeeComponent implements OnInit {
-
   employee: Employee = new Employee();
   submitted = false;
 
-  constructor(private employeeService: EmployeeService,
-    private router: Router) { }
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   newEmployee(): void {
     this.submitted = false;
@@ -25,18 +25,23 @@ export class CreateEmployeeComponent implements OnInit {
   }
 
   save() {
-    this.employeeService.createEmployee(this.employee)
-      .subscribe(data => console.log(data), error => console.log(error));
+    this.employeeService.createEmployee(this.employee).subscribe(
+      (data) => {
+        console.log(data);
+        this.gotoList();
+      },
+      (error) => console.log(error)
+    );
+
     this.employee = new Employee();
-    this.gotoList();
   }
 
   onSubmit() {
     this.submitted = true;
-    this.save();    
+    this.save();
   }
 
   gotoList() {
-    this.router.navigate(['/employees']);
+    this.router.navigate(["/employees"]);
   }
 }
--- employees-ui/src/app/employee-list/employee-list.component.ts
+++ employees-ui/src/app/employee-list/employee-list.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { Observable } from "rxjs";
+import { Observable, Subject } from "rxjs";
 import { EmployeeService } from "../employee.service";
 import { Employee } from "../employee";
-import { Router } from '@angular/router';
+import { filter, map, takeUntil } from "rxjs/operators";
+import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-employee-list',
-  templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.css']
+  selector: "app-employee-list",
+  templateUrl: "./employee-list.component.html",
+  styleUrls: ["./employee-list.component.css"],
 })
 export class EmployeeListComponent implements OnInit {
-
   employees: Observable<Employee[]>;
-
-  constructor(private employeeService: EmployeeService,
-    private router: Router) {}
+  public destroyed = new Subject<any>();
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.reloadData();
@@ -26,16 +29,21 @@ export class EmployeeListComponent implements OnInit {
   }
 
   deleteEmployee(id: number) {
-    this.employeeService.deleteEmployee(id)
-      .subscribe(
-        data => {
-          console.log(data);
-          this.reloadData();
-        },
-        error => console.log(error));
+    this.employeeService.deleteEmployee(id).subscribe(
+      (data) => {
+        console.log(data);
+        this.reloadData();
+      },
+      (error) => console.log(error)
+    );
   }
 
-  employeeDetails(id: number){
-    this.router.navigate(['details', id]);
+  employeeDetails(id: number) {
+    this.router.navigate(["details", id]);
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next();
+    this.destroyed.complete();
   }
 }
